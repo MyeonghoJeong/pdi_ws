5220eadbf8b39a4d7883d6793b8f9a1ba8bad9f8
MyeonghoJeong <jksil200@naver.com>
2024-07-24 16:13:02 +0900

    pdw_ws_v1

---
+++ src/control/src/pub_want_top.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3.8
+from typing import Any
+import rospy
+from std_msgs.msg import String, Bool, Int32
+from geometry_msgs.msg import Pose
+import tf
+
+
+def main():
+    rospy.init_node("pub_test_custom")
+    listener = tf.TransformListener()
+    while not rospy.is_shutdown():
+        top_name = input("topic name : ")
+        msg_type = input("msg type : ")
+        if msg_type == 'string':
+            pub = rospy.Publisher(top_name, String, queue_size=1)
+            data = input("data : ")
+        elif msg_type == 'bool':
+            pub = rospy.Publisher(top_name, Bool, queue_size=1)
+            data = input("data : ")
+            if data == 't':
+                data = True
+            else:
+                data = False
+        elif msg_type == 'int':
+            pub = rospy.Publisher(top_name, Int32, queue_size=1)
+            data = int(input("data : "))
+        elif msg_type == 'pose':
+            pub = rospy.Publisher(top_name, Pose, queue_size=1)
+            x = float(input('x : '))
+            y = float(input('y : '))
+            z = float(input('z : '))
+            data = Pose()
+            data.position.x = x
+            data.position.y = y
+            data.position.z = z
+
+        pub.publish(data)
+
+
+if __name__ == '__main__':
+    main()
